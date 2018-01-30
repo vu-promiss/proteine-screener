e4d13bb9b5f8d82a64f1717c1a2a84d1e7627276
abn290 <abn290@vu.nl>
2018-01-30 23:13:35 +0100

    Add info question

---
+++ src/components/InfoQuestion.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <b-row class="question">
+    <b-col cols="12">
+      <p>{{ $t(question.title) }}</p>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+    props: ['question']
+}
+</script>
