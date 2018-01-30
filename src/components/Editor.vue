a1d313d1caae35ab1fe9bc2715f858e36d0423e2
abn290 <abn290@vu.nl>
2018-01-30 23:14:24 +0100

    Add editor

---
+++ src/components/Editor.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="editor">
+    <textarea v-model="strQuestions" name="name" rows="8" cols="80"></textarea>
+    {{ strQuestions }}
+    <b-button
+      v-on:click="updateQuestions"
+    >Update</b-button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: 'screener',
+  data () {
+    return {
+      textarea: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      questions: 'quiz/questions'
+    }),
+    strQuestions () {
+      console.log(this)
+      return this.$store.getters['quiz/strQuestions']
+    }
+  },
+  methods: {
+    ...mapMutations({
+      setQuestions: 'quiz/setQuestions'
+    }),
+    updateQuestions: () => {
+      let obj = JSON.parse(this.strQuestions)
+      console.log(obj)
+    }
+  }
+}
+</script>
