91936538344cf842a09c8bd4d6716def92aa25dc
abn290 <abn290@vu.nl>
2018-01-31 22:25:08 +0100

    New quiz.json format

--- src/store/modules/quiz/actions.js
+++ src/store/modules/quiz/actions.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
 export const getQuestions = ({ commit }) => {
-  return axios.get('static/questions.json').then((response) => {
-    commit('setQuestions', response.data)
+  return axios.get('static/quiz.json').then((response) => {
+    // assemble questions
+    let questions = response.data.questions.map((obj) => {
+      if (obj.answers) {
+        obj.answers = response.data.answers[obj.answers]
+      }
+      return obj
+    })
+    commit('setQuestions', questions)
   })
 }
