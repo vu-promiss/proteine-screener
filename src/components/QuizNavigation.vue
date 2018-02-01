0c577eab143e3a1f8546aa92d809852a77ee5dd3
abn290 <abn290@vu.nl>
2018-02-01 17:54:54 +0100

    Add navigation

--- src/components/QuizNavigation.vue
+++ src/components/QuizNavigation.vue
@@ -1,20 +1,23 @@
 <template lang="html">
   <div class="quiz-navigation">
     <b-row align-h="center">
-      <b-col v-if="question.type === 'info'" class="my-3" cols="12" md="6">
-        <b-button
-            block
-            size="lg"
-            @click="nextQuestion"
-          >{{ $t('nav.continue') }}</b-button>
-      </b-col>
-      <b-col v-else class="my-3" cols="12" md="6">
-        <b-button
-            block
-            :disabled="getSelectedAnswerValue(question.id) === undefined"
-            size="lg"
-            @click="nextQuestion"
-          >{{ $t('nav.next_question') }}</b-button>
+      <b-col class="my-3" cols="12" md="6">
+        <b-button-group>
+          <b-button
+              size="lg"
+              :disabled="! prevEnabled"
+              @click="prevQuestion"
+          >
+            {{ $t('nav.prev_question') }}
+          </b-button>
+          <b-button
+              :disabled="! nextEnabled"
+              size="lg"
+              @click="nextQuestion"
+          >
+            {{ $t('nav.next_question') }}
+          </b-button>
+        </b-button-group>
       </b-col>
     </b-row>
   </div>
@@ -31,11 +34,22 @@ export default {
     ...mapGetters({
       getSelectedAnswerValue: 'quiz/getSelectedAnswer',
       currentQuestionNumber: 'quiz/currentQuestionNumber'
-    })
+    }),
+    nextEnabled () {
+      if (this.question.type === 'info') return true
+      return this.getSelectedAnswerValue(this.question.id) !== undefined
+    },
+    prevEnabled () {
+      return this.currentQuestionNumber > 1
+    }
   },
   methods: {
     nextQuestion () {
       this.$store.commit('quiz/setQuestionNumber', this.currentQuestionNumber + 1)
+      this.$store.commit('quiz/updateCurrentQuestion')
+    },
+    prevQuestion () {
+      this.$store.commit('quiz/setQuestionNumber', this.currentQuestionNumber - 1)
       this.$store.commit('quiz/updateCurrentQuestion')
     }
   }
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -3,7 +3,7 @@
     <b-row align-h="center">
       <b-col cols="12" md="6" class="text-center">
         <b-alert show >Predicted Probability: <h1>{{ predprob | decimal }}</h1></b-alert>
-        <b-button size="lg" @click="startOver">Start over</b-button>
+        <b-button size="lg" @click="startOver">{{ $t('nav.start_over') }}</b-button>
       </b-col>
     </b-row>  
   </div>
