0124d9016204c590a48a17744fa8e526fed76622
abn290 <abn290@vu.nl>
2018-02-04 10:27:11 +0100

    Use cutoff in results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,7 +2,12 @@
   <div class="results">
     <b-row align-h="center">
       <b-col cols="12" md="6" class="text-center">
-        <b-alert show >Predicted Probability: <h1>{{ predprob | decimal }}</h1></b-alert>
+        <b-alert show >
+          Predicted Probability: 
+          <h1>{{ predprob | decimal }}</h1>
+          <p v-if="predprob < cutoff"> {{ $t("sufficient_protein_intake") }} </p>
+          <p v-else> {{ $t("low_protein_intake") }} </p>
+        </b-alert>
         <b-button size="lg" @click="startOver">{{ $t('nav.start_over') }}</b-button>
       </b-col>
     </b-row>  
@@ -15,7 +20,8 @@ import { mapGetters } from 'vuex'
 export default {
   computed: {
     ...mapGetters({
-      predprob: 'quiz/predprob'
+      predprob: 'quiz/predprob',
+      cutoff: 'config/cutoff'
     })
   },
   methods: {
