a6600d54e792f6f65866f373396d660ac639e080
abn290 <abn290@vu.nl>
2018-02-05 19:11:07 +0100

    Add debug getter

--- src/store/modules/config/getters.js
+++ src/store/modules/config/getters.js
@@ -3,3 +3,5 @@ export const config = (state) => state.config
 export const locales = (state) => state.config.locales
 
 export const cutoff = (state) => state.config.cutoff
+
+export const debug = (state) => state.config.debug
