f3a714184f75fe65f4b51ca4d8760c4acac997fb
Arjen van Bochoven <nejra@me.com>
2018-04-23 10:28:20 +0200

    Change routes to use api

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+//$router->get('/results', 'ResultsController@store');
+
+
+$router->post('/api/results', 'ResultsController@store');
+$router->get('/api/export', 'ResultsController@export');
