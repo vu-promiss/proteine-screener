ee43260504a7d707c317437949b5774cf2bdc1b5
Arjen van Bochoven <nejra@me.com>
2018-05-16 11:59:35 +0200

    Add pred_prob column

---
+++ database/migrations/2018_05_16_095325_add_pred_prob_column.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPredProbColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('results', function($table)
+        {
+          $table->float('pred_prob')->nullable()->after('answers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('results', function($table)
+        {
+          $table->dropColumn('pred_prob');
+        });
+    }
+}
